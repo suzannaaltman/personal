5d06d97e36b654ea1758f6aa4bef2108211865a5
Suzanna Altman <suzanna.altman@irishtitan.com>
2017-06-20 07:34:15 -0500

    a bit of styling

--- altman.dev/wp-content/themes/altman/header.php
+++ altman.dev/wp-content/themes/altman/header.php
@@ -4,18 +4,26 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Suzanna Altman</title>
 
+    <script src="https://use.fontawesome.com/8cbb8bce79.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab|Oxygen:300,400,700" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+
 
     <?php wp_head(); ?>
   </head>
   <body>
-    <div class="container-fluid">
+    <div class="container">
       <div class="row">
-        <h1><?php echo get_bloginfo( 'name' ); ?></h1>
-        <h2><?php echo get_bloginfo( 'description' ); ?></h2>
+        <div class="col-sm-12 col-xs-10">
+          <h1><?php echo get_bloginfo( 'name' ); ?></h1>
+          <h2><?php echo get_bloginfo( 'description' ); ?></h2>
+        </div>
+        <div class="col-xs-2 visible-xs mobile-nav">
+          <div class="fa fa-bars">mobile nav</div>
+        </div>
       </div>
     </div>
 
--- altman.dev/wp-includes/template.php
+++ altman.dev/wp-includes/template.php
@@ -690,3 +690,48 @@ function load_template( $_template_file, $require_once = true ) {
 		require( $_template_file );
 	}
 }
+
+# -*- coding: utf-8 -*-
+/*
+Plugin Name: Enable Custom Fields per Default
+Version:     1.0
+Required:    3.1
+Author:      Thomas Scholz
+Author URI:  http://toscho.de
+License:     GPL
+*/
+! defined( 'ABSPATH' ) and exit;
+add_filter( 'default_hidden_meta_boxes', 'enable_custom_fields_per_default', 20, 1 );
+
+/**
+ * Removes custom fields from the default hidden elements.
+ *
+ * The original ( wp-admin/includes/template.php#get_hidden_meta_boxes() ):
+ * array(
+ *      'slugdiv',
+ *      'trackbacksdiv',
+ *      'postcustom',      <-- we need this
+ *      'postexcerpt',
+ *      'commentstatusdiv',
+ *      'commentsdiv',
+ *      'authordiv',
+ *      'revisionsdiv'
+ * )
+ *
+ * It has no effect if the user has decided to hide the box.
+ * This option is saved in "metaboxhidden_{$screen->id}"
+ *
+ * @param  array $hidden
+ * @return array $hidden
+ */
+function enable_custom_fields_per_default( $hidden )
+{
+    foreach ( $hidden as $i => $metabox )
+    {
+        if ( 'postcustom' == $metabox )
+        {
+            unset ( $hidden[$i] );
+        }
+    }
+    return $hidden;
+}
